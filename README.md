64710821b07cf30871b208b3dfe04b09fbc6c408
Danila Loginov <danila@loginov.rocks>
2018-01-28 23:44:51 +0300

    feat: implement Comments Extractor class

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# comments-extractor
-Extract comments from glob of files, annotated or not
+# Comments Extractor
+
+[![dependencies Status](https://david-dm.org/1oginov/comments-extractor/status.svg)](https://david-dm.org/1oginov/comments-extractor)
+[![devDependencies Status](https://david-dm.org/1oginov/comments-extractor/dev-status.svg)](https://david-dm.org/1oginov/comments-extractor?type=dev)
+
+Extract comments from glob of files, annotated or not.
+
+## Quick Start
+
+Install package:
+
+```sh
+npm install comments-extractor
+```
+
+Use in code:
+
+```js
+const CommentsExtractor = require('comments-extractor');
+const extractor = new CommentsExtractor('./gulp/*.js', 'todo');
+const todos = extractor.extract();
+```
---
+++ src/CommentsExtractor.js
@@ -0,0 +1,95 @@
+const esprima = require('esprima');
+const fs = require('fs');
+const glob = require('glob');
+
+/**
+ * Comments Extractor class.
+ */
+class CommentsExtractor {
+  /**
+   * Comments Extractor constructor.
+   * @param {string} pattern Glob pattern
+   * @param {string} [annotation=''] Annotation
+   * @param {Object} [options={}] Glob options
+   * @param {*} [esprimaClass=esprima]
+   * @param {*} [fsClass=fs]
+   * @param {*} [globClass=glob]
+   */
+  constructor(pattern, annotation = '', options = {}, esprimaClass = esprima,
+              fsClass = fs, globClass = glob) {
+    this.annotation = annotation;
+    this.esprima = esprimaClass;
+    this.fs = fsClass;
+    this.glob = globClass;
+    this.options = options;
+    this.pattern = pattern;
+  }
+
+  /**
+   * Extract comments.
+   * @return {Map<string, Array>}
+   */
+  extract() {
+    const files = this.getFiles(this.pattern, this.options);
+    const filesToComments = new Map;
+
+    files.map((file) => {
+      const data = this.getFileData(file);
+      let comments = this.getComments(data);
+
+      if (this.annotation.length > 0) {
+        comments = this.filterAnnotatedComments(comments, this.annotation);
+      }
+
+      filesToComments.set(file, comments);
+    });
+
+    return filesToComments;
+  }
+
+  /**
+   * Filter annotated comments only.
+   * @param {Array} comments
+   * @param {string} annotation
+   * @return {Array}
+   */
+  filterAnnotatedComments(comments, annotation) {
+    return comments.filter((comment) => comment.value.toLowerCase().
+        indexOf('@' + annotation.toLowerCase()) >= 0);
+  }
+
+  /**
+   * Get comments.
+   * @param {string} data
+   * @param {*} [esprima=this.esprima]
+   * @return {Array}
+   */
+  getComments(data, esprima = this.esprima) {
+    return esprima.parseModule(data, {
+      comment: true,
+    }).comments;
+  }
+
+  /**
+   * Get file data.
+   * @param {string} path
+   * @param {*} [fs=this.fs]
+   * @return {string}
+   */
+  getFileData(path, fs = this.fs) {
+    return fs.readFileSync(path, 'utf-8');
+  }
+
+  /**
+   * Get files fitting pattern.
+   * @param {string} pattern
+   * @param {Object} [options={}]
+   * @param {*} [glob=this.glob]
+   * @return {Array}
+   */
+  getFiles(pattern, options = {}, glob = this.glob) {
+    return glob.sync(pattern, options);
+  }
+}
+
+module.exports = CommentsExtractor;
